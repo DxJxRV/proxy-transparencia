c6d3d4834bdc60be22f9e57a66ea7dc09435c016
DxJx <diegovalentin663@gmail.com>
2025-12-05 09:08:34 -0600

    [add] ssg

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const DB_CONFIG = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || 'rootroot',
   database: process.env.DB_NAME || 'transparencia_db'
 };
 
